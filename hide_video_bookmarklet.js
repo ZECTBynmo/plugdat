58c95478287a80740896fdf5e6fb33d95cf64db4
ZECTBynmo <michael.vegeto@gmail.com>
2013-12-03 15:40:40 -0500

    make the hide video bookmarklet point to the correct script

--- hide_video_bookmarklet.js
+++ hide_video_bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){$('body').append('%3Cscript%20src=\'https://raw.github.com/ZECTBynmo/plugdat/master/autobop.js\'%3E%3C/script%3E');})();
+javascript:(function(){$('body').append('%3Cscript%20src=\'https://raw.github.com/ZECTBynmo/plugdat/master/hideVideo.js\'%3E%3C/script%3E');})();
