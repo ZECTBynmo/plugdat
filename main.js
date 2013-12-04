3298044059127ecc95f71e80ad5493221b5aea05
ZECTBynmo <michael.vegeto@gmail.com>
2013-12-04 14:36:19 -0500

    Make things a little fancier

--- main.js
+++ main.js
@@ -65,91 +65,54 @@ PlugDat.prototype.cleanUp = function() {
 
 
 PlugDat.prototype.setupChatHandlers = function() {
-	API.on(API.CHAT_COMMAND, callback); function callback(value) {
-		switch( value ) {
-			// ---------------------------
-			// function commands
-			// ---------------------------
-			case '/show':
-		        $("#playback").slideDown();
-		        break;
-	        case '/hide':
-		        $("#playback").slideUp();
-		        break;
-	        case '/hideall':
-	       	 	$("#playback").slideUp();
-		        $("#audience").hide()
-		        $("#dj-booth").hide()
-		        break;
-	        case '/showall':
-	       	 	$("#playback").slideDown();
-		        $("#audience").show()
-		        $("#dj-booth").show()
-		        break;
-
-			// ---------------------------
-			// Emojj
-			// ---------------------------
-		    case '/whatever':
-		        API.sendChat( '¯\\_(ツ)_/¯' );
-		        break;
-		    case '/tableflip':
-		        API.sendChat( '(╯°□°）╯︵ ┻━┻' );
-		        break;
-	        case '/tablefix':
-		        API.sendChat( '\┬\─\┬﻿ ノ( ゜-゜ノ)' );
-		        break;
-	        case '/seriousface':
-	        	API.sendChat( 'ಠ_ಠ' );
-	        	break;
-	    	case '/monocle':
-	        	API.sendChat( 'ಠ_ರೃ' );
-	        	break;
-	        case '/cat':
-	            API.sendChat( '(=ↀωↀ=)' );
-	            break;
-	        case '/fastcat':
-	            API.sendChat( '(ↀДↀ)⁼³₌₃' );
-	            break;
-	        case '/deadcat':
-	            API.sendChat( '(=ｘェｘ=)' );
-	            break;
-	        case '/sleepycat':
-	            API.sendChat( '(≚ᄌ≚)ƶƵ' );
-	            break;
-	        case '/happycat':
-	            API.sendChat( 'ヽ(=^･ω･^=)丿' );
-	            break;
-            case '/pootcat':
-	            API.sendChat( '(ↀДↀ)⁼³₌₃' );
-	            break;
-	        case '/jazzhands':
-	            API.sendChat( 'ლ(=ↀωↀ=)ლ' );
-	            break;
-	        case '/woof':
-	            API.sendChat( '(U・x・U)' );
-	            break;
-	        case '/squid':
-	            API.sendChat( '＜コ：ミ' );
-	            break;
-	        case '/fish':
-	            API.sendChat( '❥᷁)͜͡˒ ⋊' );
-	            break;
-	        case '/snowman':
-	            API.sendChat( '⁝⁝⸃₍⁽΄˙̥΄ ⁾₎⸜☂');
-	            break;
-	        case '/wizard':
-	            API.sendChat( '(∩｀-´)⊃━☆ﾟ.*･｡ﾟ' );
-	            break;
-	        case '/raiseyourdongers':
-	            API.sendChat( 'ヽ༼ ಠ益ಠ ༽ﾉ' );
-	            break;
-	        case '/fuckyou':
-	            API.sendChat( '凸ಠ益ಠ)凸' );
-	            break;
+	API.on( API.CHAT, function(value) {
+		for( var iCommand in commands ) {
+			if( value.indexOf(iCommand) != -1 ) {
+				if( typeof(commands[iCommand]) == "string" )
+					API.sendChat( commands[iCommand] );
+				else
+					commands[iCommand];
+			}
 		}
 	}
 }
+
+var commands = {
+	"/show": function() {
+		$("#playback").slideDown();
+	},
+	"/hide": function() {
+		$("#playback").slideUp();
+	},
+	"/hideall": function() {
+		$("#playback").slideUp();
+        $("#audience").hide()
+        $("#dj-booth").hide()
+	},
+	"/showall": function() {
+		$("#playback").slideDown();
+        $("#audience").show()
+        $("#dj-booth").show()
+	},
+	"/whatever": 			"¯\\_(ツ)_/¯",
+	"/tableflip": 			"(╯°□°）╯︵ ┻━┻",
+	"/tablefix": 			"\┬\─\┬﻿ ノ( ゜-゜ノ)",
+	"/seriousface": 		"ಠ_ಠ",
+	"/cat": 				"(=ↀωↀ=)",
+	"/fastcat": 			"(ↀДↀ)⁼³₌₃",
+	"/deadcat": 			"(=ｘェｘ=)",
+	"/sleepycat": 			"(≚ᄌ≚)ƶƵ",
+	"/happycat": 			"ヽ(=^･ω･^=)丿",
+	"/pootcat": 			"(ↀДↀ)⁼³₌₃",
+	"/jazzhands": 			"ლ(=ↀωↀ=)ლ",
+	"/woof": 				"(U・x・U)",
+	"/squid": 				"＜コ：ミ",
+	"/fish": 				"❥᷁)͜͡˒ ⋊",
+	"/snowman": 			"⁝⁝⸃₍⁽΄˙̥΄ ⁾₎⸜☂",
+	"/wizard": 				"(∩｀-´)⊃━☆ﾟ.*･｡ﾟ",
+	"/raiseyourdongers": 	"ヽ༼ ಠ益ಠ ༽ﾉ",
+	"/fuckyou": 			"凸ಠ益ಠ)凸",
+};
 
 
 // -----------------------------------------------------------------
