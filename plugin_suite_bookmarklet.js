b77d38010a9d7930202459fa6bd996f597fcca07
Devers Talmage <deverstalmage@gmail.com>
2013-12-03 21:15:43 -0500

    forgot to add the full plugin suite bookmarklet

---
+++ plugin_suite_bookmarklet.js
@@ -0,0 +1,4 @@
+javascript:(function(){$('body').append(
+        '%3Cscript%20src=\'https://raw.github.com/ZECTBynmo/plugdat/master/TTMessages.js\'%3E%3C/script%3E'+
+        '%3Cscript%20src=\'https://raw.github.com/ZECTBynmo/plugdat/master/hideVideo.js\'%3E%3C/script%3E'+
+        '%3Cscript%20src=\'https://raw.github.com/ZECTBynmo/plugdat/master/autobop.js\'%3E%3C/script%3E');})();
